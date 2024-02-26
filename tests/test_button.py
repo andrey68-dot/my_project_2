fbdfe68e7c272f1b23e21236b0b6ec67f9eec1bc
Andrey <andrey.chudinov68andrei@yandex.ru>
2024-02-26 16:46:39 +0500

    first test

--- tests/test_button.py
+++ tests/test_button.py
@@ -10,3 +10,5 @@ def browser():
 def test_button_exist(browser):
     browser.get('https://www.qa-practice.com/elements/button/simple')
     assert browser.find_element(By.ID, 'submit-id-submit').is_displayed()
+
+#Всем привет
