199292213336fc5651fbfdac256cb8fd4737f813
Andrey <andrey.chudinov68andrei@yandex.ru>
2024-02-27 21:05:40 +0500

    first action

--- tests/test_button.py
+++ tests/test_button.py
@@ -1,15 +1,4 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-import pytest
-
-# @pytest.fixture()
-# def browser():
-#     option = Options()
-#     option.add_argument('--headless')
-#     chrome_browser = webdriver.Chrome(options=option)
-#     return chrome_browser
-
 def test_button_exist(browser):
     browser.get('https://www.qa-practice.com/elements/button/simple')
     assert browser.find_element(By.ID, 'submit-id-submit').is_displayed()
