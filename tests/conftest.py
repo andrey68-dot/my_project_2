8e7bb320fa8dcd7cf9cda5a408bb8bb71ae9269a
Andrey <andrey.chudinov68andrei@yandex.ru>
2024-02-27 20:02:58 +0500

    first action

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+
+
+@pytest.fixture()
+def browser():
+    option = Options()
+    option.add_argument('--headless')
+    chrome_browser = webdriver.Chrome(options=option)
+    return chrome_browser
