33a234d9b9232a76b6d781b2fcfc727b8fb4292c
Andrey <andrey.chudinov68andrei@yandex.ru>
2024-03-01 11:30:53 +0500

    new

--- tests/test_button.py
+++ tests/test_button.py
@@ -5,3 +5,7 @@ import pytest
 def test_button_exits(browser):
     browser.get('https://www.qa-practice.com/elements/button/simple')
     assert browser.find_element(By.ID, 'submit-id-submit').is_displayed()
+
+def test_button_exits_2(browser):
+    browser.get('https://www.qa-practice.com/elements/button/like_a_button')
+    assert browser.find_element(By.PARTIAL_LINK_TEXT, 'Click').is_displayed()
